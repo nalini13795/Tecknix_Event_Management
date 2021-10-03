d40c17062c1b7a96f30be0d636ec16934538cf48
nalini13795 <nalinikatiyar@gmail.com>
2021-10-02 21:45:36 -0400

    Converting HTML to EJS template

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const morgan = require('morgan');
+var methodOverride = require('method-override')
+
+const app = express();  
+let host = 'localhost';
+let port = 3000;
+
+app.set('view engine','ejs');
+
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('tiny'));
+app.use(methodOverride('_method'))
+
+
+
+app.get('/',(req,res)=>{
+    res.render('index');
+})
+
+app.use((req,res,next)=>{
+    let err = new Error('Server cannot locate '+req.url);
+    err.status = 404;
+    next(err); 
+});
+app.use((err,req,res,next) => {
+    console.log(err.stack);
+    if(!err.status){
+        err.status = 500;
+        err.message = ("Internal Server Error");
+    }
+    res.status(err.status)
+    res.render('error',{error : err});
+});
+
+app.listen(port, host, ()=>{
+    console.log('App running on the port ',port);
+})
